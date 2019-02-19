53f32dc03c8750325e02a8567ec40ed91cc76ea1
Karthik Kunjithapatham <karthikk@Karthiks-MacBook-Pro.local>
2019-02-20 10:27:07 +1100

    Initial commit

---
+++ src/main/java/com/kk/spring5restwebflux/Spring5RestWebfluxApplication.java
@@ -0,0 +1,13 @@
+package com.kk.spring5restwebflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5RestWebfluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5RestWebfluxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kk/spring5restwebflux/Spring5RestWebfluxApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kk.spring5restwebflux;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Spring5RestWebfluxApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
